5816d8b46080e4afdce2483a93686ee39f55741a
softdeveloper23 <brannongarrett77@gmail.com>
2024-06-15 14:09:14 +0200

    First commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.next();
+        String str2 = scanner.next();
+        String str3 = scanner.next();
+        String str4 = scanner.next();
+        String str5 = scanner.next();
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(str4);
+        System.out.println(str5);
+    }
+}
+
